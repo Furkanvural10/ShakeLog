615ecd4a3b2f8cd23135d34a91b8aad07046b848
Furkan Vural <62665073+Furkanvural10@users.noreply.github.com>
2026-01-02 23:24:49 +0300

    Add view model protocol

--- ShakeLog/Sources/ShakeLog/Presentation/UIKit/LogViewModel.swift
+++ ShakeLog/Sources/ShakeLog/Presentation/UIKit/LogViewModel.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
+@available(iOS 13.0.0, *)
+protocol LogViewModelInterface {
+    
+    var view: LogViewControllerInterface? { get set }
+
+    func handleViewDidLoad() async
+}
 
 
+@available(iOS 13.0.0, *)
+final class LogViewControllerModel {
+    weak var view: (any LogViewControllerInterface)?
+}
+
+@available(iOS 13.0.0, *)
+extension LogViewControllerModel: LogViewModelInterface {
+    
+    func handleViewDidLoad() async {
+        
+        await view?.setupUI()
+        
+    }
+}
