a38baac373706611806873853d1d392575bef711
Furkan Vural <62665073+Furkanvural10@users.noreply.github.com>
2026-01-07 23:17:52 +0300

    Add AccessibilityIDs file

---
+++ Sources/ShakeLog/Core/Utils/AccessibilityIDs/AccessibilityIDs.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  ShakeLog
+//
+//  Created by furkan vural on 7.01.2026.
+//
+
+import Foundation
+
+
+struct AccessibilityIDs {
+    static let logViewController = "logViewController"
+    static let logViewControllerTitle = "logViewControllerTitle"
+    static let logTableView = "logTableView"
+}
