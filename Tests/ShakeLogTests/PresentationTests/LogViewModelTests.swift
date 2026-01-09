81b8abd2928cc92025b608c61e578b91b5937e29
Furkan Vural <62665073+Furkanvural10@users.noreply.github.com>
2026-01-09 23:30:40 +0300

    Add unit test folder

---
+++ Tests/ShakeLogTests/PresentationTests/LogViewModelTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import ShakeLog
+
+final class LogViewModelTests: XCTestCase {
+
+    
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
